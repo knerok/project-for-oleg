5f37c5c665aa4f2bdf584ee03c5bc9261477f7d5
Andre Berto <knerok@gmail.com>
2019-06-02 19:10:39 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-import app from './app';
+import $ from 'jquery';
+import './style.css';
 
-app();
+$(document).ready(() => {
+  $('#submitButton').click(() => {
+    const phoneNumber = $('#phoneNumber').val();
+    console.log(phoneNumber);
+    $.ajax({
+      type: 'POST',
+      url: 'insert.php',
+      data: {
+        phoneNumber,
+      },
+      success() {
+        const $d = $(`<li>${phoneNumber}</li>`).fadeIn(500, () => {
+          $('#phoneNumber').val('');
+        });
+        $('ul#list').prepend($d);
+      },
+    });
+  });
+});
